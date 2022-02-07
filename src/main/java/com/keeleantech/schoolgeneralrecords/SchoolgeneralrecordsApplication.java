aec7f7298d771cdb5fa075f9aad0c3ee06d26a5f
Keelean <cmkslee@gmail.com>
2022-02-07 15:01:40 +0100

    Initial commit

---
+++ src/main/java/com/keeleantech/schoolgeneralrecords/SchoolgeneralrecordsApplication.java
@@ -0,0 +1,13 @@
+package com.keeleantech.schoolgeneralrecords;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchoolgeneralrecordsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SchoolgeneralrecordsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/keeleantech/schoolgeneralrecords/SchoolgeneralrecordsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.keeleantech.schoolgeneralrecords;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchoolgeneralrecordsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
